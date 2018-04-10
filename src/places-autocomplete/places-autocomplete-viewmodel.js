2da78ea7f7fd6760424c53471ea34fc1c28a6c17
cherif BOUCHELAGHEM <cherifbouchelaghem@yahoo.fr>
2018-04-10 15:32:49 +0100

    fix click suggestion

--- src/places-autocomplete/places-autocomplete-viewmodel.js
+++ src/places-autocomplete/places-autocomplete-viewmodel.js
@@ -4,28 +4,74 @@ import PlaceModel from "./place-model";
 import { find } from "lodash/core";
 
 export default DefineMap.extend({
+	/**
+	 *
+	 */
+	restriction: "any",
+
+	/**
+	 *
+	 **/
 	suggestions: {
-		Type: DefineList,
-		default() {
-			return new DefineList([])
-		}
-	},
-	place: {
-		type: {
+		type: [{
 		  placeId: "string",
-			description: "string",
+		 	description: "string",
 		  index: "number",
 		  active: "boolean",
 		  formattedSuggestion: "string"
+		}],
+		default() {
+			return new DefineList([]);
 		},
-		set(place) {
-			this.clearSuggestions();
-			return place;
+		value(suggestions) {
+			
+			suggestions.listenTo(suggestions.lastSet, suggestions.resolve);
+
+			suggestions.listenTo("place", () => {
+				this.clearSuggestions();
+			});
+
+			suggestions.resolve(new DefineList());
 		}
 	},
 
-	placesComponentRestriction: "any",
+	/**
+	 *
+	 **/
+	place: {
+		type: {
+		  placeId: "string",
+		 	description: "string",
+		  index: "number",
+		  active: "boolean",
+		  formattedSuggestion: "string"
+		}
+	},
 
+	/**
+	 *
+	 **/
+	description: {
+		type: 'string',
+		value: function(description) {
+			
+			description.listenTo("place", (ev, place) => {
+				description.resolve(place.description)
+			});
+
+			description.listenTo("activeSuggestion", (ev,suggestion) => {
+				if (suggestion) {
+					description.resolve(suggestion.description);
+				}
+			});
+
+			description.resolve(null);
+		}
+	},
+
+	/**
+	 *
+	 **/
 	get activeSuggestion() {
 		let activeSuggestion = find(this.suggestions, (suggestion) => {
 			return suggestion.active;
@@ -33,25 +79,19 @@ export default DefineMap.extend({
 		return activeSuggestion;
 	},
 
-	get description() {
-		let description = '';
-		if (this.place) {
-			description = this.place.description;
-		}
-
-		if (this.activeSuggestion) {
-			description = this.activeSuggestion.description;
-		}
-		return description;
-	},
-
+	/**
+	 *
+	 **/
 	selectActiveDescriptionAt(idx) {
-		let activeDescription = _.find(this.suggestions, (suggestion) => {
+		let activeDescription = find(this.suggestions, (suggestion) => {
 			return suggestion.index === idx;
 		}).description;
 		this.activateSuggestionAt(idx);
 	},
 
+	/**
+	 *
+	 **/
 	activateSuggestionAt(idx) {
 		this.suggestions = this.suggestions.map((suggestion, index) => {
 			if (idx === index) {
@@ -63,14 +103,23 @@ export default DefineMap.extend({
 		});
 	},
 
+	/**
+	 *
+	 **/
 	selectActiveSuggestion() {
 		this.place = this.activeSuggestion;
 	},
 
+	/**
+	 *
+	 **/
 	clearSuggestions() {
 		this.suggestions = new DefineList();
 	},
 
+	/**
+	 *
+	 **/
 	selectPlace(suggestion) {
 		this.place = suggestion;
 	}
